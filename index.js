03721bd7a3a94bccba05eb48e09dd6bb41275c4f
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2021-05-01 18:20:16 +0900

    add get nicname function
    입력창에 기입한 닉네임 가져온다.
    테스트를 위해 경고창에 입력한 닉네임이 나온다.
    입력창에서 엔터를 누르거나 마우스로 enter btn을 클릭하면 함수가 호출된다.

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-// // 이벤트 발생
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-//   socket.on('chat message', (msg) => {
-//     io.emit('chat message', msg);
-//   });
-//   socket.on('disconnect', () => {
-//   console.log('user disconnected');
-//   });
-// });
-const port = 12000
-http.listen(port, () => {
-  console.log('Connected at', port);
-  console.log()
-});
+var nickname = "";
+var img = "";
+
+function enter(){
+  getNickName();
+  // main.html로 img와 nickname 정보와 함꼐넘어간다.
+}
+
+function getNickName() {
+  nickname = $("#input_box").val();
+  alert(nickname);
+}
